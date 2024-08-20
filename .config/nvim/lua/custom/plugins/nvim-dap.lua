148e697633da7f4cf68d0efc86ce1c35c0de923f
Moussa Haidous <haidous.m@gmail.com>
2024-08-20 11:33:26 -0700

    remove dap-ui

--- .config/nvim/lua/custom/plugins/nvim-dap.lua
+++ .config/nvim/lua/custom/plugins/nvim-dap.lua
@@ -1,16 +1,12 @@
 return {
-	-- NOTE: Yes, you can install new plugins here!
 	"mfussenegger/nvim-dap",
-	-- NOTE: And you can specify dependencies as well
 	dependencies = {
-		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
 		"williamboman/mason.nvim",
 		"jay-babu/mason-nvim-dap.nvim",
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
 		require("mason-nvim-dap").setup({
 			automatic_installation = true,
 			handlers = {},
@@ -31,37 +27,6 @@ return {
 			dap.repl.toggle()
 		end, { desc = "Debug: Toggle REPL" })
 
-		-- Dap UI setup
-		-- For more information, see |:help nvim-dap-ui|
-		-- dapui.setup({
-		-- 	-- Set icons to characters that are more likely to work in every terminal.
-		-- 	--    Feel free to remove or use ones that you like more! :)
-		-- 	--    Don't feel like these are good choices.
-		-- 	icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-		-- 	controls = {
-		-- 		icons = {
-		-- 			pause = "⏸",
-		-- 			play = "▶",
-		-- 			step_into = "⏎",
-		-- 			step_over = "⏭",
-		-- 			step_out = "⏮",
-		-- 			step_back = "b",
-		-- 			run_last = "▶▶",
-		-- 			terminate = "⏹",
-		-- 			disconnect = "⏏",
-		-- 		},
-		-- 	},
-		-- })
-		--
-		-- -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		-- vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
-		--
-		-- dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		-- dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		-- dap.listeners.before.event_exited["dapui_config"] = dapui.close
-		dap.listeners.after["event_terminated"]["nvim-metals"] = function()
-			dap.repl.open()
-		end
 		-- Dap / Metals integration
 		dap.configurations.scala = {
 			{
@@ -81,5 +46,8 @@ return {
 				},
 			},
 		}
+		dap.listeners.after["event_terminated"]["nvim-metals"] = function()
+			dap.repl.open()
+		end
 	end,
 }
