b4670bf7c4989585f0f46d2fe7840c2e160bf3a5
Moussa Haidous <haidous.m@gmail.com>
2024-10-29 12:40:35 -0700

    add oil

---
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
